2429761ed12701401ba1961d3a9e95e414250d43
Mạnh <nguyentangmanh180502@gmail.com>
2024-05-06 22:06:55 +0700

    Create main 2

---
+++ src/main/java/DoWhhileDemo.java
@@ -0,0 +1,9 @@
+public class DoWhhileDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println(i++);
+            System.out.println("mạnh");
+        }while (i <= 10);
+    }
+}
